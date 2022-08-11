050979104e8ae803b8f0e995b9483062e9bf68b4
Haniful-Islam <haniful360@gmail.com>
2022-08-12 01:28:17 +0600

    country name and flag added

--- js/script.js
+++ js/script.js
@@ -5,6 +5,18 @@ const loadCountries = () =>{
 }
 
 const displayCountries = countries =>{
-    console.log(countries);
+    // console.log(countries);
+    const allCountriesHTML = countries.map(country =>getCountris(country))
+    console.log(allCountriesHTML);
+    const container = document.getElementById('countries');
+    container.innerHTML = allCountriesHTML.join(' ');
+}
+const getCountris = country =>{
+    return`
+        <div class = "country">
+            <h2>${country.name.common}</h2>
+            <img src='${country.flags.png}'>
+        </div>
+    `
 }
 loadCountries()
